bc3c215925f4bd68d33267e1b568a9628eac0cb2
jeferson <jeferson.silva2@estudante.ifto.edu.br>
2025-06-22 01:45:01 -0300

    v2.1

--- src/SQL/BD.sql
+++ src/SQL/BD.sql
@@ -1,13 +1,67 @@
 create database vidaSerena;
+
 use vidaSerena;
 
 create table cadastroUsers(
-id int auto_increment unique,
-nome varchar(250) not null,
-cpf int not null,
-telefone int not null,
-emial varchar(250) not null,
-senha1 varchar(100) not null,
-senha2 varchar(100) not null,
-arquivo longblob
+    id int auto_increment primary key,
+    nome varchar(250) not null,
+    cpf int not null unique,
+    telefone int not null unique,
+    email varchar(250) not null unique,
+    senha varchar(100) not null,
+    arquivo longblob
+);
+
+create table cadastroFamilia(
+    id int auto_increment primary key,
+    id_usuario int,
+    tipoParentesco varchar(100) not null,
+    endereco varchar(150) not null,
+    foreign key (id_usuario) references cadastroUsers(id)
+);
+
+create table cadastroCuidador(
+    id int auto_increment primary key,
+    id_usuario int,
+    cursos varchar(250) not null,
+    foreign key (id_usuario) references cadastroUsers(id)
+);
+
+create table cadastroEnfermeiro(
+    id int auto_increment primary key,
+    id_usuario int,
+    coren varchar(30) not null,
+    cip varchar(30) not null,
+    foreign key (id_usuario) references cadastroUsers(id)
+);
+
+create table cadastroMedico(
+    id int auto_increment primary key,
+    id_usuario int,
+    crm varchar(30) not null,
+    foreign key (id_usuario) references cadastroUsers(id)
+);
+
+create table cadastroMIdoso(
+    id int auto_increment primary key,
+    id_usuario int,
+    resposavel varchar(150) not null,
+    condicoesMedicas varchar(250) not null,
+    medicamentosUso varchar(250) not null,
+    resticoesAlimentar varchar(250) not null,
+    alergias varchar(250) not null,
+    foreign key (id_usuario) references cadastroUsers(id)
+);
+
+create table cadastroMedicamentos(
+    id int auto_increment primary key,
+    tipoMedicamento enum(
+        'Sem tarja',
+        'Tarja amarela',
+        'Tarja vermelha',
+        'Tarja preta'
+    ) not null,
+    quantDeCaixa int not null,
+    quantPorCaixa int not null,
+    arquivo longblob
 );
