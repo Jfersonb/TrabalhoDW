20327cb3b32fdf0112650e3b943bed91e7c1a278
jeferson <jeferson.silva2@estudante.ifto.edu.br>
2025-06-25 20:23:17 -0300

    v3.3

--- src/PHP/Password.php
+++ src/PHP/Password.php
@@ -3,7 +3,7 @@
 $senha = 'senha123';
 $senhaHash = password_hash($senha, PASSWORD_DEFAULT);
 
-// Exemplo de uso com MySQLi
+
 $sql = "INSERT INTO cadastroUsers (nome, cpf, telefone, email, senha, perfil) VALUES (?, ?, ?, ?, ?, ?)";
 $stmt = $conn->prepare($sql);
 $stmt->bind_param("ssssss", $nome, $cpf, $telefone, $email, $senhaHash, $perfil);
